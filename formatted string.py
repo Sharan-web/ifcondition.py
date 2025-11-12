e3e6b80064e8ca00e670e06b152b8eb8fc5a31e0
sharan-web <sharansharu03167@gmail.com>
2025-11-12 10:19:24 +0530

    Create formatted string.py

---
+++ formatted string.py
@@ -0,0 +1,6 @@
+name="sharan"
+age=18
+college="siet"
+deparment="AIML"
+print(f"my name is: {name} amd my age is: {age} and  i am studing at: {college} and in deparment of: {deparment} ")
+my name is: sharan amd my age is: 18 and  i am studing at: siet and in deparment of: AIML 
