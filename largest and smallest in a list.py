c4c86657229093037ae0191029e6dfcf858a14d8
Sharan-web <sharansharu03167@gmail.com>
2025-11-14 12:29:04 +0530

    Create largest and smallest in a list.py

---
+++ largest and smallest in a list.py
@@ -0,0 +1,10 @@
+#largest and smallest elements in a list
+numbers = [4,8,1,9,3]
+
+largest =max(numbers)
+smallest = min(numbers)
+
+print("largest=",largest)
+print("smalest=",smallest)
+largest= 9
+smalest= 1
