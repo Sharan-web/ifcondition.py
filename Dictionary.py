27de3f950e6e13232542fb39d70f78c20e0ece09
Sharan-web <sharansharu03167@gmail.com>
2025-11-14 10:07:57 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+#dictionary
+sharan={
+    "age" :18,
+    "weight" :50,
+    "height" :6.1,
+    "department" :"aiml",
+    "from" :"hospet",
+
+}
+print(sharan)
+{'age': 18, 'weight': 50, 'height': 6.1, 'department': 'aiml', 'from': 'hospet'}
