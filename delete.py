aeb6320d2ab74a96581de811cfd820a3d82dc355
Sharan-web <sharansharu03167@gmail.com>
2025-11-14 10:16:11 +0530

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,12 @@
+sharan={
+    "age" :18,
+    "weight" :50,
+    "height" :6.1,
+    "department" :"aiml",
+    "from" :"hospet",
+} 
+del sharan["age"]
+del sharan["weight"]
+print(sharan)
+
+{'height': 6.1, 'department': 'aiml', 'from': 'hospet'}
