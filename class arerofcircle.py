ac0c97767fcfe577ba0bac096b24d245eb035ad4
Sharan-web <sharansharu03167@gmail.com>
2025-11-15 11:34:46 +0530

    Create class arerofcircle.py

---
+++ class arerofcircle.py
@@ -0,0 +1,9 @@
+class areaofcircle:
+  def __init__(self,radius):
+   self.radius=radius
+  def function(self):
+    return 3.14*self.radius*self.radius 
+obj = areaofcircle(5)
+obj.function()
+
+78.5
